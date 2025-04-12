17ba4ee31188c8c057a42617fd0d034b72a75a7f
jeven <m.milan@stud.uni-obuda.hu>
2025-04-13 00:33:40 +0200

    Falf of Backend

---
+++ BACKEND/villany_szamla/Consuption.cs
@@ -0,0 +1,39 @@
+﻿namespace villany_szamla
+{
+    public class Consuption
+    {
+        public static int[] AddArray(int[] array)
+        {
+            if (array.Length != 12)
+            {
+                throw new ArgumentException("Only 12 length array accepted");
+            }
+            return array;
+        }
+
+        public static int GetFirstYear(string input)
+        {
+            string[] stripped = input.Split("\n");
+            return int.Parse(stripped[0].Split(",")[0]);
+        }
+
+        public static List<double[]> GetListOfYears(double price, string input)
+        {
+            string[] stripped = input.Split("\n");
+            List<double[]> res = new List<double[]>();
+            for (int i = 1; i < stripped.Length; i++)
+            {
+                double[] temp = new double[12];
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    temp[j] = double.Parse(stripped[i].Split(",")[j])*price+23.4 ;
+                }
+                res.Add(temp);
+            }
+            return res;
+        }
+
+        public int StartYear { get; set; }
+        public List<double[]> ListOfYears { get; set; }
+    }
+}
---
+++ BACKEND/villany_szamla/Controllers/ConsuptionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace villany_szamla.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConsuptionController: ControllerBase
+    {
+        private readonly ILogger<ConsuptionController> _logger;
+
+        public ConsuptionController(ILogger<ConsuptionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetConsuption")]
+        public IEnumerable<Consuption> Get(double price)
+        {
+            string input = "2000,2001\n1,2,3,4,5,6,7,8,9,10,11,12\n1,2,3,4,5,6,7,8,9,10,11,12";
+            return Enumerable.Range(1, 1).Select(index => new Consuption
+            {
+                StartYear = Consuption.GetFirstYear(input),
+                ListOfYears = Consuption.GetListOfYears(price,input),
+                }
+            ).ToArray();
+        }
+    }
+}
